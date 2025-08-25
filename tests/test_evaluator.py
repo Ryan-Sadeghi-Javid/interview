89f337b823e6cb36e2d6a33059c8a6fa88a15216
Joanna Moon <joanna.moon05@gmail.com>
2025-08-25 19:24:01 -0400

    worlde

--- tests/test_evaluator.py
+++ tests/test_evaluator.py
@@ -3,4 +3,12 @@ from src.evaluator import Evaluator
 def test_basic_greens_and_yellows():
     ev = Evaluator()
     # They must implement duplicate-aware logic to make this pass
-    assert ev.evaluate("trace", "track") == ['G','G','G','.','.']
+    assert ev.evaluate("trace", "track") == ['G','G','G','G','.']
+
+def test_yellows():
+    ev = Evaluator()
+    assert ev.evaluate("cider", "reidc") == ['Y', 'Y', 'Y', 'Y', 'Y']
+
+def test_period():
+    ev = Evaluator()
+    assert ev.
